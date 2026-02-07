b6051e0a872e0e26b7efd6904297d74868f75706
idgafabyou <idgafabyou21@gmail.com>
2026-02-08 00:06:18 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,11 @@
-
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("breezy-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/style.css",
+        "/app.js"
+      ]);
+    })
+  );
+});
