6de353ac22e9808fa7fc5228c52cdcf0ad1b91b1
idgafabyou <idgafabyou21@gmail.com>
2026-02-08 07:38:43 +0100

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const TransactionSchema = new mongoose.Schema({
+  userId: String,
+  type: String,
+  amount: Number,
+  description: String,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
